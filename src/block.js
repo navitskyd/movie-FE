18951e0a3783090500873ab6d2a5371dcd8c8e1b
Dzmitry_Navitski2 <navitskyd@yandex.by>
2021-06-09 00:21:20 +0300

    webpack setup

--- src/block.js
+++
@@ -1,9 +0,0 @@
-import React, { PureComponent } from "react";
-
-class Block extends PureComponent {
-    render() {
-        return <div>Pure Componennt</div>;
-    }
-}
-
-export default Block;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+//import _ from 'lodash';
+//
+// function component() {
+//   const element = document.createElement('div');
+//
+//  // Lodash, now imported by this script
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//
+//   return element;
+// }
+//
+// document.body.appendChild(component());
 
-const rootElement = document.getElementById("root");
-
-ReactDOM.render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
-    rootElement
-);
+import ReactDOM from 'react-dom';
+import React from 'react';
+const App = () => {
+ return <h1>This is my React app!</h1>;
+ }
+ReactDOM.render(<App />, document.getElementById('app'));
