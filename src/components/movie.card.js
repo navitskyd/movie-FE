b28582c36f79e73327c4b05dd91806b09e491e9b
Dzmitry_Navitski2 <navitskyd@yandex.by>
2021-06-09 23:05:32 +0300

    styling

---
+++ src/components/movie.card.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function MovieCard(){
+return (
+<>
+<h3>Movie Title</h3>
+<p>Description</p>
+</>
+)
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-//import _ from 'lodash';
-//
-// function component() {
-//   const element = document.createElement('div');
-//
-//  // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//
-//   return element;
-// }
-//
-// document.body.appendChild(component());
-
 import ReactDOM from 'react-dom';
 import React from 'react';
+import MovieCard from 'components/movie.card';
 const App = () => {
- return <h1>This is my React app!</h1>;
+ return (
+ <>
+ <h1>This is my React app!</h1>
+ <MovieCard />
+ <MovieCard />
+ <MovieCard />
+  </>
+ )
  }
 ReactDOM.render(<App />, document.getElementById('app'));
+
