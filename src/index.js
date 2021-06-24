ef0a04f57046957385d83bf790271024373f76a5
Dzmitry_Navitski2 <navitskyd@yandex.by>
2021-06-24 13:55:40 +0300

    resolve comments

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-//import _ from 'lodash';
-//
-// function component() {
-//   const element = document.createElement('div');
-//
-//  // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//
-//   return element;
-// }
-//
-// document.body.appendChild(component());
-
 import ReactDOM from 'react-dom';
 import React from 'react';
+
 const App = () => {
- return <h1>This is my React app!</h1>;
- }
-ReactDOM.render(<App />, document.getElementById('app'));
+    return <h1>This is my React app!</h1>;
+}
+ReactDOM.render(<App/>, document.getElementById('app'));
