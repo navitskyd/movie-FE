8b44ab509390e063f09c29cc3e8a7f6c01a50c26
Dzmitry_Navitski2 <navitskyd@yandex.by>
2021-06-25 14:16:43 +0300

    used typescript interface

--- src/app/components/movie.card/movie.card.tsx
+++ src/app/components/movie.card/movie.card.tsx
@@ -1,35 +1,22 @@
 import * as React from 'react'
-import * as PropTypes from 'prop-types'
 import './movie.card.css'
 import MovieCover from "./movie.cover";
 import MovieTitle from "./movie.title";
 import MovieDescription from "./movie.description";
 import MovieYear from "./movie.year";
+import {MovieData} from "./movie.data";
 
 
-export default function MovieCard(props) {
+export default function MovieCard(data: MovieData) {
     return (
         <div className='MovieCard'>
-            <MovieCover cover={props.cover}/>
+            <MovieCover cover={data.cover}/>
             <div className="under-cover">
-                <MovieTitle title={props.title}/>
-                <MovieYear year={props.year}/>
+                <MovieTitle title={data.title}/>
+                <MovieYear year={data.year}/>
             </div>
-            <MovieDescription description={props.description}/>
+            <MovieDescription description={data.description}/>
         </div>
     )
 }
 
-MovieCard.propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    year: PropTypes.number,
-    cover: PropTypes.string,
-
-}
-MovieCard.defaultProps = {
-    title: "Just a Perfect Movie",
-    description: "Cool description",
-    year: 0,
-    cover: ""
-}
